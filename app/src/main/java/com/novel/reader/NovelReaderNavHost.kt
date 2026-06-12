d545024c7aeeccebe9c2c4f44d45dee097ddb31e
NovelReader <ouyanghui02@gmail.com>
2026-06-13 01:45:55 +0800

    Fix: add dp import to NavHost

--- app/src/main/java/com/novel/reader/NovelReaderNavHost.kt
+++ app/src/main/java/com/novel/reader/NovelReaderNavHost.kt
@@ -10,6 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
