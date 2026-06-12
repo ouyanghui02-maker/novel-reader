a133317efda4e03613b342fc0f01f2642ef6ae28
NovelReader <ouyanghui02@gmail.com>
2026-06-13 01:54:41 +0800

    Fix: add Modifier import back

--- app/src/main/java/com/novel/reader/NovelReaderNavHost.kt
+++ app/src/main/java/com/novel/reader/NovelReaderNavHost.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
