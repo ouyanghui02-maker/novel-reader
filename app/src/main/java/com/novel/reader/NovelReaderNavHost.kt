2e8faff782302e99d1121cbbd8de9acf76426ede
NovelReader <ouyanghui02@gmail.com>
2026-06-13 01:48:41 +0800

    Fix: resolve import conflicts in NavHost

--- app/src/main/java/com/novel/reader/NovelReaderNavHost.kt
+++ app/src/main/java/com/novel/reader/NovelReaderNavHost.kt
@@ -8,9 +8,8 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavDestination.Companion.hierarchy
