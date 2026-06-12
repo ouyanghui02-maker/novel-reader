c2661a0a76361add815f6d52287b5c5f76fdb1e1
NovelReader <ouyanghui02@gmail.com>
2026-06-13 01:52:14 +0800

    Fix: remove redundant ImageVector and Modifier imports

--- app/src/main/java/com/novel/reader/NovelReaderNavHost.kt
+++ app/src/main/java/com/novel/reader/NovelReaderNavHost.kt
@@ -9,9 +9,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
