f57a8f50e4d49f059cf3707c9d046ca5488063b8
NovelReader <ouyanghui02@gmail.com>
2026-06-12 14:12:16 +0800

    Fix: restore SearchRule/BookInfoRule/TocRule/ContentRule classes

--- core/model/src/main/java/com/novel/model/BookSource.kt
+++ core/model/src/main/java/com/novel/model/BookSource.kt
@@ -16,3 +16,47 @@ data class BookSource(
     val ruleToc: String = "{}",
     val ruleContent: String = "{}"
 )
+
+data class SearchRule(
+    val bookList: String = "",
+    val name: String = "",
+    val author: String = "",
+    val coverUrl: String = "",
+    val bookUrl: String = "",
+    val wordCount: String = "",
+    val lastChapter: String = "",
+    val intro: String = "",
+    val kind: String = ""
+)
+
+data class BookInfoRule(
+    val init: String = "",
+    val name: String = "",
+    val author: String = "",
+    val coverUrl: String = "",
+    val intro: String = "",
+    val wordCount: String = "",
+    val lastChapter: String = "",
+    val tocUrl: String = ""
+)
+
+data class TocRule(
+    val preUpdateJs: String = "",
+    val preUpdateAppLog: String = "",
+    val postUpdateJs: String = "",
+    val postUpdateAppLog: String = "",
+    val chapterList: String = "",
+    val nextTocUrl: String = "",
+    val isVolume: String = ""
+)
+
+data class ContentRule(
+    val preUpdateJs: String = "",
+    val preUpdateAppLog: String = "",
+    val postUpdateJs: String = "",
+    val postUpdateAppLog: String = "",
+    val content: String = "",
+    val replaceRegex: List<String> = emptyList(),
+    val nextContentUrl: String = "",
+    val payAction: String = ""
+)
