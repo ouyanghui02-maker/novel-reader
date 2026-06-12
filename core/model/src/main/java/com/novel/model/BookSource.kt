10f00c30aa8764acf68a46db8347a1a84e5bc80d
NovelReader <ouyanghui02@gmail.com>
2026-06-12 14:03:27 +0800

    Simplify BookSource entity, store rules as JSON strings

--- core/model/src/main/java/com/novel/model/BookSource.kt
+++ core/model/src/main/java/com/novel/model/BookSource.kt
@@ -2,139 +2,17 @@ package com.novel.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "book_sources")
 data class BookSource(
     @PrimaryKey
-    @SerializedName("bookSourceUrl")
     val bookSourceUrl: String = "",
-    
-    @SerializedName("bookSourceName")
     val bookSourceName: String = "",
-    
-    @SerializedName("bookSourceGroup")
     val bookSourceGroup: String = "",
-    
-    @SerializedName("enabled")
     val enabled: Boolean = true,
-    
-    @SerializedName("searchUrl")
     val searchUrl: String = "",
-    
-    @SerializedName("ruleSearch")
-    val ruleSearch: SearchRule = SearchRule(),
-    
-    @SerializedName("ruleBookInfo")
-    val ruleBookInfo: BookInfoRule = BookInfoRule(),
-    
-    @SerializedName("ruleToc")
-    val ruleToc: TocRule = TocRule(),
-    
-    @SerializedName("ruleContent")
-    val ruleContent: ContentRule = ContentRule()
-)
-
-data class SearchRule(
-    @SerializedName("bookList")
-    val bookList: String = "",
-    
-    @SerializedName("name")
-    val name: String = "",
-    
-    @SerializedName("author")
-    val author: String = "",
-    
-    @SerializedName("coverUrl")
-    val coverUrl: String = "",
-    
-    @SerializedName("bookUrl")
-    val bookUrl: String = "",
-    
-    @SerializedName("wordCount")
-    val wordCount: String = "",
-    
-    @SerializedName("lastChapter")
-    val lastChapter: String = "",
-    
-    @SerializedName("intro")
-    val intro: String = "",
-    
-    @SerializedName("kind")
-    val kind: String = ""
-)
-
-data class BookInfoRule(
-    @SerializedName("init")
-    val init: String = "",
-    
-    @SerializedName("name")
-    val name: String = "",
-    
-    @SerializedName("author")
-    val author: String = "",
-    
-    @SerializedName("coverUrl")
-    val coverUrl: String = "",
-    
-    @SerializedName("intro")
-    val intro: String = "",
-    
-    @SerializedName("wordCount")
-    val wordCount: String = "",
-    
-    @SerializedName("lastChapter")
-    val lastChapter: String = "",
-    
-    @SerializedName("tocUrl")
-    val tocUrl: String = ""
-)
-
-data class TocRule(
-    @SerializedName("preUpdateJs")
-    val preUpdateJs: String = "",
-    
-    @SerializedName("preUpdateAppLog")
-    val preUpdateAppLog: String = "",
-    
-    @SerializedName("postUpdateJs")
-    val postUpdateJs: String = "",
-    
-    @SerializedName("postUpdateAppLog")
-    val postUpdateAppLog: String = "",
-    
-    @SerializedName("chapterList")
-    val chapterList: String = "",
-    
-    @SerializedName("nextTocUrl")
-    val nextTocUrl: String = "",
-    
-    @SerializedName("isVolume")
-    val isVolume: String = ""
-)
-
-data class ContentRule(
-    @SerializedName("preUpdateJs")
-    val preUpdateJs: String = "",
-    
-    @SerializedName("preUpdateAppLog")
-    val preUpdateAppLog: String = "",
-    
-    @SerializedName("postUpdateJs")
-    val postUpdateJs: String = "",
-    
-    @SerializedName("postUpdateAppLog")
-    val postUpdateAppLog: String = "",
-    
-    @SerializedName("content")
-    val content: String = "",
-    
-    @SerializedName("replaceRegex")
-    val replaceRegex: List<String> = emptyList(),
-    
-    @SerializedName("nextContentUrl")
-    val nextContentUrl: String = "",
-    
-    @SerializedName("payAction")
-    val payAction: String = ""
+    val ruleSearch: String = "{}",
+    val ruleBookInfo: String = "{}",
+    val ruleToc: String = "{}",
+    val ruleContent: String = "{}"
 )
